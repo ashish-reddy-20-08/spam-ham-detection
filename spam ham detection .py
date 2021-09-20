74f7afd2a018cec08feb489e905fe76cccc211c4
M. Ashish Reddy <ashishreddy2222@gmail.com>
2021-09-20 20:51:40 +0530

    Update spam ham detection .py

--- spam ham detection .py
+++ spam ham detection .py
@@ -4,7 +4,7 @@
 # In[1]:
 
 
-#importing the required packages 
+#importing the required packages.
 import pandas as pd 
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer 
